11655b55cf467c3a64c60636ddf1f24f0c987b63
Artyom <petrakovartyom@gmail.com>
2024-11-24 02:14:54 +0300

    task done

--- graph.py
+++ graph.py
@@ -1,10 +1,20 @@
+import json
+import os
 import xml.etree.ElementTree as ET
 from graphviz import Digraph
 
-def parse_dependencies(file_path):
+
+def load_config(config_path):
+    with open(config_path, 'r') as file:
+        return json.load(file)
+
+
+def parse_dependencies(file_path, max_depth):
     tree = ET.parse(file_path)
     root = tree.getroot()
+
     ns = {'mvn': 'http://maven.apache.org/POM/4.0.0'}
+
     dependencies = []
     for dependency in root.findall('.//mvn:dependency', ns):
         group_id = dependency.find('mvn:groupId', ns).text
@@ -16,21 +26,17 @@ def parse_dependencies(file_path):
             'version': version
         })
 
-    return dependencies
+    return dependencies[:max_depth]
 
-def create_graph(dependencies, output_file='dependencies_graph'):
+
+def create_graph(dependencies, package_name, output_file, graphviz_path):
+    os.environ["PATH"] = f"{os.environ.get('PATH', '')}:{graphviz_path}"
     dot = Digraph(format='png', engine='dot')
+    dot.node("Project", package_name, shape="box")
+
     for dep in dependencies:
         node_label = f"{dep['groupId']}:{dep['artifactId']}\n{dep['version']}"
         dot.node(node_label, node_label)
         dot.edge("Project", node_label)
 
-    # Добавляем корневой проект
-    dot.node("Project", "Project", shape="box")
-
-    # Сохраняем и выводим граф
-    dot.render(output_file, view=True)
-
-file_path = 'pom.xml'
-dependencies = parse_dependencies(file_path)
-create_graph(dependencies)
+    dot.render(output_file, view=False)
